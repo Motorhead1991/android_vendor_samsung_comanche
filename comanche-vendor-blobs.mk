ad8f6c492c94fb568aa8931132f2e1ee761b7c87
Mike C <mcrosson_git at nusku dot net>
2012-11-20 20:23:29 +0000

    Updated Adreno libs and proprietary files to match

--- comanche-vendor-blobs.mk
+++ comanche-vendor-blobs.mk
@@ -47,6 +47,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/comanche/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
 	vendor/samsung/comanche/proprietary/lib/libgsl.so:system/lib/libgsl.so \
 	vendor/samsung/comanche/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
+    vendor/samsung/comanche/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
 	vendor/samsung/comanche/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
 	vendor/samsung/comanche/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
 	vendor/samsung/comanche/proprietary/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
